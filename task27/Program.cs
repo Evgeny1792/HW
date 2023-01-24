7aca361277b04b453d298dda20166b0edfd01840
Evgeny1792 <121222846+Evgeny1792@users.noreply.github.com>
2023-01-24 19:29:26 +0700

    Add files via upload

---
+++ task27/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a number: ");
+        string input = Console.ReadLine();
+        int n;
+        if (int.TryParse(input, out n))
+        {
+            int sum = 0;
+            while (n != 0)
+            {
+                sum += n % 10;
+                n /= 10;
+            }
+            Console.WriteLine("The sum of digits in the number is: " + sum);
+        }
+        else
+        {
+            Console.WriteLine("Invalid input, please enter a number.");
+        }
+    }
+}
