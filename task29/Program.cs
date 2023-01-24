8f72fda1d8826d80bcd05b8af31eb3a98a0f414a
Evgeny1792 <121222846+Evgeny1792@users.noreply.github.com>
2023-01-24 19:42:23 +0700

    Add files via upload
    task 29

---
+++ task29/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = new int[8]; // Declaring an array of 8 elements
+        Console.WriteLine("Enter 8 numbers:");
+        for (int i = 0; i < 8; i++)
+        {
+            array[i] = int.Parse(Console.ReadLine()); // Assign user input to each element of the array
+        }
+        Console.Write("The elements of the array are: [");
+        for (int i = 0; i < 8; i++)
+        {
+            if (i == 7) {
+              Console.Write(array[i]);
+            } else {
+              Console.Write(array[i] + ", ");
+            }
+        }
+        Console.Write("]");
+    }
+}
