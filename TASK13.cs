698536ef24b17d9c318b9e8d40a74b5d9bd6c802
Evgeny1792 <121222846+Evgeny1792@users.noreply.github.com>
2023-01-24 11:00:20 +0700

    Add files via upload

---
+++ TASK13.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number;
+        Console.WriteLine("Enter a number: ");
+        number = Convert.ToInt32(Console.ReadLine());
+
+        string strNum = number.ToString();
+        if (strNum.Length >= 3)
+        {
+            char thirdDigit = strNum[2];
+            Console.WriteLine("The third digit of the number is: " + thirdDigit);
+        }
+        else
+        {
+            Console.WriteLine("The number doesn't have a third digit.");
+        }
+    }
+}
---
+++ TASK15.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int day;
+        Console.WriteLine("Enter a number representing a day of the week (1-7): ");
+        day = Convert.ToInt32(Console.ReadLine());
+
+        if (day == 6 || day == 7)
+        {
+            Console.WriteLine("It's a weekend day!");
+        }
+        else
+        {
+            Console.WriteLine("It's a weekday.");
+        }
+    }
+}
