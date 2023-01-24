3f1796b2564a910dba629f0742aec72acd2b1bfa
Evgeny1792 <121222846+Evgeny1792@users.noreply.github.com>
2023-01-24 10:39:59 +0700

    Add files via upload

---
+++ TASK10.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program {
+    static void Main() {
+        Console.WriteLine("Enter a three-digit number:");
+        int num = Convert.ToInt32(Console.ReadLine());
+        int secondDigit = (num % 100) / 10;
+        Console.WriteLine("The second digit of the number is: " + secondDigit);
+    }
+}
