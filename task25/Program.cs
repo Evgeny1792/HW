079d0c707d9bc3be4ec856fb509748be734c08a9
Evgeny1792 <121222846+Evgeny1792@users.noreply.github.com>
2023-01-24 19:17:06 +0700

    hw

---
+++ task25/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a number: ");
+        string inputA = Console.ReadLine();
+        int a;
+        if (int.TryParse(inputA, out a))
+        {
+            Console.WriteLine("Enter a exponent: ");
+            string inputB = Console.ReadLine();
+            int b;
+            if (int.TryParse(inputB, out b) && b > 0)
+            {
+                int result = 1;
+                for (int i = 1; i <= b; i++)
+                {
+                    result *= a;
+                }
+                Console.WriteLine(a + " raised to the power of " + b + " is: " + result);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input, please enter a positive number.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input, please enter a number.");
+        }
+    }
+}
+
