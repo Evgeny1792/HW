5428b23234aa2c063fe6c586fe7cafa91482521d
Evgeny1792 <121222846+Evgeny1792@users.noreply.github.com>
2023-01-30 21:25:40 +0700

    task34
    task34

---
+++ task34/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = new int[8];
+        Random random = new Random();
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = random.Next(100, 1000);
+        }
+
+        int evenCount = GetEvenCount(numbers);
+
+        Console.WriteLine("The number of even numbers in the array is: " + evenCount);
+    }
+
+    static int GetEvenCount(int[] numbers)
+    {
+        int count = 0;
+
+        foreach (int number in numbers)
+        {
+            if (number % 2 == 0)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
