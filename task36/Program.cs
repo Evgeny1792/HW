72404b20500014ac2b409d2a3093c839e22563a6
Evgeny1792 <121222846+Evgeny1792@users.noreply.github.com>
2023-01-30 21:31:25 +0700

    task36
    36

---
+++ task36/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OddSumArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            Random random = new Random();
+
+            Console.WriteLine("Original Array: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next(100);
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    sum += arr[i];
+                }
+            }
+
+            Console.WriteLine("Sum of elements at odd positions: " + sum);
+        }
+    }
+}
