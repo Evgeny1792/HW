86f80cbd834d0fba17a534a926d4a2144e917e39
Evgeny1792 <121222846+Evgeny1792@users.noreply.github.com>
2023-02-03 16:28:34 +0700

    hw
    hw

---
+++ task47/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the number of rows in the matrix: ");
+        int m = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the number of columns in the matrix: ");
+        int n = int.Parse(Console.ReadLine());
+
+        double[,] matrix = new double[m, n];
+        Random rnd = new Random();
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                matrix[i, j] = Math.Round(rnd.NextDouble() * 10, 1);
+            }
+        }
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
