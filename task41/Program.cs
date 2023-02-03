65b2605510f422b921d93ea260fe6af70b993eea
Evgeny1792 <121222846+Evgeny1792@users.noreply.github.com>
2023-02-03 14:50:37 +0700

    hw
    41

---
+++ task41/Program.cs
@@ -0,0 +1,22 @@
+﻿
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter numbers: ");
+            string input = Console.ReadLine();
+            string[] inputNumbers = input.Split(',');
+            int count = 0;
+
+            foreach (var number in inputNumbers)
+            {
+                if (int.Parse(number) > 0)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine("Number of numbers greater than 0: " + count);
+        }
+    }
+
