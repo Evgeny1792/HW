f2b051968c88a72c4dc38b969b9c9b7c2afc5016
Evgeny1792 <121222846+Evgeny1792@users.noreply.github.com>
2023-02-13 14:43:29 +0700

    hw

---
+++ task66/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter values for M and N: ");
+            int m = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            int sum = GetSum(m, n);
+            Console.WriteLine("Sum of numbers from M to N: " + sum);
+        }
+
+        static int GetSum(int m, int n)
+        {
+            if (m == n)
+                return m;
+            return m + GetSum(m + 1, n);
+        }
+    }
+}
+
