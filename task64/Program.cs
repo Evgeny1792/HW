43acd8aedd5100d985546b838d3b850adeb6031f
Evgeny1792 <121222846+Evgeny1792@users.noreply.github.com>
2023-02-13 14:42:49 +0700

    hw

---
+++ task64/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a value for N: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Numbers from N to 1: ");
+            PrintNumbers(n);
+            Console.WriteLine();
+        }
+
+        static void PrintNumbers(int n)
+        {
+            if (n == 0)
+                return;
+
+            Console.Write(n + " ");
+            PrintNumbers(n - 1);
+        }
+    }
+}
