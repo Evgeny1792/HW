61aa0da4adb074ed95c9152759d9d8507488709e
Evgeny1792 <121222846+Evgeny1792@users.noreply.github.com>
2023-03-24 12:54:39 +0700

    final
    final work

---
+++ final/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Введите массив строк
+        string[] inputStrings = { "hello", "world", "my", "name", "is", "GeekBrains" };
+
+        // Создайте новый массив для строк длиной не более 3 символов
+        string[] outputStrings = new string[inputStrings.Length];
+        int outputIndex = 0;
+
+        // Пройдитесь по каждой строке и добавьте её в новый массив, если её длина меньше или равна 3 символам
+        foreach (string inputString in inputStrings)
+        {
+            if (inputString.Length <= 3)
+            {
+                outputStrings[outputIndex] = inputString;
+                outputIndex++;
+            }
+        }
+
+        // Выведите результаты
+        Console.WriteLine("Input strings: {0}", string.Join(", ", inputStrings));
+        Console.WriteLine("Output strings: {0}", string.Join(", ", outputStrings));
+    }
+}
